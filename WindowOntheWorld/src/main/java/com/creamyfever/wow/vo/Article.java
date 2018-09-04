fb44a138b5ff9c9bd9eb5202ceff3658774e4b48
Taehee Lee <taehee1408@gmail.com>
2018-09-04 19:06:10 +0900

    Drop column 'field' from table 'articles'

--- WindowOntheWorld/src/main/java/com/creamyfever/wow/vo/Article.java
+++ WindowOntheWorld/src/main/java/com/creamyfever/wow/vo/Article.java
@@ -7,7 +7,6 @@ public class Article {
 	private String articlecontent; // 기사 내용
 	private String articledate; // 기사 날짜
 	private String presscompany; // 언론사
-	private String field; // 분야(정치, 경제, 스포츠, IT 등...)
 	private String url; // URL
 	private String continent; // 대륙(아시아, 유럽, 아메리카, 아프리카...(null일 경우 DB에는 아시아로 등록)
 	private double sentiment; // 긍정적 반응 비율(0~100)
@@ -17,7 +16,7 @@ public class Article {
 	}
 
 	public Article(int articlenum, String articleid, String articlename, String articlecontent, String articledate,
-			String presscompany, String field, String url, String continent, double sentiment) {
+			String presscompany, String url, String continent, double sentiment) {
 		super();
 		this.articlenum = articlenum;
 		this.articleid = articleid;
@@ -25,7 +24,6 @@ public class Article {
 		this.articlecontent = articlecontent;
 		this.articledate = articledate;
 		this.presscompany = presscompany;
-		this.field = field;
 		this.url = url;
 		this.continent = continent;
 		this.sentiment = sentiment;
@@ -79,14 +77,6 @@ public class Article {
 		this.presscompany = presscompany;
 	}
 
-	public String getField() {
-		return field;
-	}
-
-	public void setField(String field) {
-		this.field = field;
-	}
-
 	public String getUrl() {
 		return url;
 	}
@@ -115,7 +105,6 @@ public class Article {
 	public String toString() {
 		return "Article [articlenum=" + articlenum + ", articleid=" + articleid + ", articlename=" + articlename
 				+ ", articlecontent=" + articlecontent + ", articledate=" + articledate + ", presscompany="
-				+ presscompany + ", field=" + field + ", url=" + url + ", continent=" + continent + ", sentiment="
-				+ sentiment + "]";
+				+ presscompany + ", url=" + url + ", continent=" + continent + ", sentiment=" + sentiment + "]";
 	}
 }
